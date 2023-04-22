6ad6c4abd1d1406d0012454031c13f480948d213
Abidur24 <123297812+Abidur24@users.noreply.github.com>
2023-04-22 22:42:00 +0600

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,25 @@
+
+// In the name of Allah.
+// We're nothing and you're everything.
+// Ya MAALIK-UL-MULK!!!
+/*
+
+Indeed, my prayer, my rites of sacrifice,
+my living and my dying are for Allah, Lord of the worlds
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,m,k;
+    cin >>n>>m>>k;
+    if(n<=m && n<=k)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
