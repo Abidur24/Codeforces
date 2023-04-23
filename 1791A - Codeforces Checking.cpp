9c15da98ca924a92945e87bfdac575d34642666c
Abidur24 <123297812+Abidur24@users.noreply.github.com>
2023-04-24 03:06:02 +0600

    Create 1791A - Codeforces Checking.cpp

---
+++ 1791A - Codeforces Checking.cpp
@@ -0,0 +1,28 @@
+// In the name of Allah.
+// We're nothing and you're everything.
+// Ya MAALIK-UL-MULK!!!
+/*
+
+Indeed, my prayer, my rites of sacrifice,
+my living and my dying are for Allah, Lord of the worlds
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    char ch;
+    for(int i=1;i<=t;i++)
+    {
+        cin>>ch;
+        if(ch=='c'||ch=='o'||ch=='d'||ch=='e'||ch=='f'||ch=='o'||ch=='r'||ch=='e'||ch=='s')
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
