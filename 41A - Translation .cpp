e14a040193a48986cffce5eea54ee5ee05d9efdf
Abidur24 <123297812+Abidur24@users.noreply.github.com>
2023-04-28 18:12:02 +0600

    Create 41A - Translation .cpp

---
+++ 41A - Translation .cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s,rs;
+    cin>>s>>rs;
+    reverse(rs.begin(),rs.end());
+    if(s==rs)
+    {
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+
+}
