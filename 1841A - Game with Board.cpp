320769bac97080d74fba535171a3c68ed8389bab
Abidur24 <123297812+Abidur24@users.noreply.github.com>
2023-06-13 13:31:44 +0600

    Create 1841A - Game with Board

---
+++ 1841A - Game with Board.cpp
@@ -0,0 +1,33 @@
+// In the name of Allah.
+// We're nothing and you're everything.
+// Ya MAALIK-UL-MULK!!!
+/*
+
+Indeed, my prayer, my rites of sacrifice,
+my living and my dying are for Allah, Lord of the worlds
+
+ */
+#include<bits/stdc++.h>
+#define ll long long
+#define f for
+#define cndl cout<<endl
+using namespace std;
+int main()
+{
+    ll int t;
+    cin>>t;
+    while(t--)
+    {
+        int n ;
+        cin>>n;
+        if(n<5)
+        {
+            cout<<"Bob"<<endl;
+        }
+        else
+        {
+            cout<<"Alice"<<endl;
+        }
+    }
+    return 0;
+}
