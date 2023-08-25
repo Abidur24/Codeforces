ea709f99ec2f44a75694b4b7df803a87e7631f72
Abidur24 <123297812+Abidur24@users.noreply.github.com>
2023-08-25 22:32:15 +0600

    Create 1812A - Are You a Robot?

---
+++ 1812A - Are You a Robot?.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define FASTIO  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define cndl cout<<endl
+#define endl "\n"
+using namespace std;
+int main()
+{
+FASTIO
+	cout<<"security"<<endl;
+	return 0;
+}
