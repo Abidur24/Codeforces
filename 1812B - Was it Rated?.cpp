4752bdb925adb19bdc6635f9dd355e00bc7d3f54
Abidur24 <123297812+Abidur24@users.noreply.github.com>
2023-08-25 22:31:36 +0600

    Create 1812B - Was it Rated?

---
+++ 1812B - Was it Rated?.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define FASTIO  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define cndl cout<<endl
+#define endl "\n"
+using namespace std;
+int main()
+{
+FASTIO
+	int n;
+	cin>>n;
+	if(n==15 or n==20 or n==21) cout<<"NO"<<endl;
+	else cout<<"YES"<<endl;
+	return 0;
+}
